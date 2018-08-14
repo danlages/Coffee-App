1ccfbd55dad8d7522180bbdca7f94aada282287a
Sophie Traynor <sophietraynor9@hotmail.com>
2018-08-14 15:36:23 +0100

    sample data on extras screen sorted in sections

---
+++ To-Go/AddExtrasTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  AddExtrasTableViewCell.swift
+//  To-Go
+//
+//  Created by Sophie Traynor on 14/08/2018.
+//  Copyright © 2018 To-Go. All rights reserved.
+//
+
+import UIKit
+
+class AddExtrasTableViewCell: UITableViewCell {
+
+    //MARK: - Properties
+    
+    @IBOutlet weak var extra: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
