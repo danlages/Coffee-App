b4672aae3687385779988c71083aac5e3d027b6d
Sophie Traynor <sophietraynor9@hotmail.com>
2018-08-21 15:36:55 +0100

    store item with selected extras in array

---
+++ To-Go/Models/Order.swift
@@ -0,0 +1,31 @@
+//
+//  Order.swift
+//  To-Go
+//
+//  Created by Sophie Traynor on 21/08/2018.
+//  Copyright © 2018 To-Go. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Order {
+    
+    //MARK: Declaration
+    
+    var order: [[String]]
+    
+    //MARK: Initialization
+    
+    init?(order: [[String]])
+    {
+        guard !order.isEmpty else //guard statement: Must be true for code after statement to be executed
+        {
+            return nil // at least 1 item is required in order
+        }
+        
+        self.order = order
+        
+    }
+    
+}
