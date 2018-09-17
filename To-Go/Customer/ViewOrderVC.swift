3c173968479dae2300f4be4f5d050d26c767276d
Sophie Traynor <sophietraynor9@hotmail.com>
2018-09-17 15:07:36 +0100

    dummy layout for view order

--- To-Go/Customer/ViewOrderVC.swift
+++ To-Go/Customer/ViewOrderVC.swift
@@ -9,6 +9,12 @@
 
 import UIKit
 
+struct itemCell{
+    var open = Bool()
+    var item = String()
+    var extras = [String]()
+}
+
 class ViewOrderVC: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     
     //MARK: Properties
@@ -18,6 +24,8 @@ class ViewOrderVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
     var menuItems  = [MenuItem]()  //Creates a mutable array of menu item objects - allowing for the addition of items after initilsation
     
     var order = [Order]()
+    
+    var tableItemCells = [itemCell]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,29 +78,37 @@ class ViewOrderVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
     
     private func loadSampleMenuItems() {
         
-        guard let menuItem1 = MenuItem(name: "Coffee", size: "Small", price: 2) else{
+        /*guard let menuItem1 = MenuItem(name: "Coffee", size: "Small", price: 2) else{
             
             fatalError("Unable to create the training ground menu item") //Error message
         }
         
-        menuItems += [menuItem1]
+        menuItems += [menuItem1]*/
+        
+        tableItemCells = [itemCell(open: false, item: "TestItem1", extras: ["extra1", "extra2"]),
+                          itemCell(open: false, item: "TestItem2", extras: ["extra1"]),
+                          itemCell(open: false, item: "TestItem3", extras: ["extra1", "extra2", "extra3"])]
         
     }
     // MARK: Table view data source
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return tableItemCells.count
     }
     
    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return menuItems.count
+        if tableItemCells[section].open == true {
+            return tableItemCells[section].extras.count + 1
+        } else {
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cellIdentifier = "SelectItemTableViewCell" //Name used to recognise cell prototype - set in attributes inspector
         
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? SelectItemTableViewCell else {
+        /*guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? SelectItemTableViewCell else {
             
             fatalError("The Dequeued cell is not an instance of SelectItemTableViewCell.")
         }
@@ -105,11 +121,47 @@ class ViewOrderVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
         cell.menuItemSize.text = menuItem.size
         cell.menuItemPrice.text = String(menuItem.price)
         
-        return cell
+        return cell*/
+        
+        let dataIndex = indexPath.row - 1
+        
+        if indexPath.row == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? SelectItemTableViewCell else {
+                
+                fatalError("The Dequeued cell is not an instance of SelectItemTableViewCell.")
+            }
+            cell.menuItemName.text = tableItemCells[indexPath.section].item
+            return cell
+            
+        } else {
+            /*guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? SelectItemTableViewCell else {
+                
+                fatalError("The Dequeued cell is not an instance of SelectItemTableViewCell.")
+            }
+            cell.menuItemName.text = tableItemCells[indexPath.section].extras[dataIndex]*/
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cellExtras") else {return UITableViewCell()}
+            cell.textLabel?.text = tableItemCells[indexPath.section].extras[dataIndex]
+            return cell
+        }
     }
     
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        displayActionSheet() //Display delete option to user via action sheet when cell selected
+        //displayActionSheet() //Display delete option to user via action sheet when cell selected
+        if indexPath.row == 0 {
+            if tableItemCells[indexPath.section].open == true{
+                tableItemCells[indexPath.section].open = false
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none)
+            } else {
+                tableItemCells[indexPath.section].open = true
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none)
+            }
+        } else {
+            //something to happen when an extra is clicked?
+            //remove if indexPath.row == 0 if want to close extras when clicked
+        }
+        
     }
 }
