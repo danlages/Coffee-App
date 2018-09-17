ba4ccbac9412175176263b7ca3bc6fca59d49378
DanLages <danlages65@gmail.com>
2018-09-17 22:56:39 +0100

    WIP
    WIP

--- To-Go/Customer/UpdateExtrasVC.swift
+++ To-Go/Customer/UpdateExtrasVC.swift
@@ -13,4 +13,18 @@ import FirebaseFirestore
 
 class UpdateExtrasVC: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        loadExtras()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
