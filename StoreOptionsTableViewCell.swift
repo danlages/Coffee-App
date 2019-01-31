ed3e180c0c2a33ef45835e812ac7077e787d349d
DanLages <danlages65@gmail.com>
2019-01-31 10:44:33 +0000

    Business UI Implementation
    The addition of new View Controllers in order to implement intital Buisness UI designs.

---
+++ StoreOptionsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoreOptionsTableViewCell.swift
+//  To-Go
+//
+//  Created by Dan Lages on 31/01/2019.
+//  Copyright © 2019 To-Go. All rights reserved.
+//
+
+import UIKit
+
+class StoreOptionsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
