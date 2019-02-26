2294934ee7b793f8da1c031ad376dea200cfc5ef
Daniel Lages <c1875958@cardiff.ac.uk>
2019-02-26 22:02:20 +0000

    Payment UI Implementation
    Implementation of Payment Details User Interface with required segues added.

--- To-Go/Customer/PaymentDetailsVC.swift
+++ To-Go/Customer/PaymentDetailsVC.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class PaymentDetailsVC: UIViewController {
 
+     var minsToCollect = 0
 
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! ActiveOrder
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        let dataData = formatter.string(from: Date() + TimeInterval(minsToCollect))
+        
+        destination.setTimeForCollection = "Collect at: " + dataData
+        destination.selectedMinutes = minsToCollect
+    }
+    
     
 
     /*
@@ -30,7 +40,4 @@ class PaymentDetailsVC: UIViewController {
     }
     */
 
-   
-    
-    
 }
