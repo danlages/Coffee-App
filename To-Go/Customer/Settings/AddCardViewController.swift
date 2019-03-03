d6667276dd1a47f901ad853a4e3cc48e1f547ec7
Daniel Lages <c1875958@cardiff.ac.uk>
2019-03-03 12:14:26 +0000

    Inital Payment Method Persist Data Structure
    Implementation of Payment Method data Structure and class.

--- To-Go/Customer/Settings/AddCardViewController.swift
+++ To-Go/Customer/Settings/AddCardViewController.swift
@@ -12,11 +12,15 @@ class AddCardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.userInputPresent), name: UIApplication.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.userInputEnded), name: UIApplication.keyboardDidHideNotification, object: nil)
 
         // Do any additional setup after loading the view.
     }
     
-    @IBOutlet weak var addCardTableViewController: UITableView!
+    @IBOutlet weak var addCardScrollView: UIScrollView!
+    @IBOutlet weak var AddCardContainerView: UIView!
     
     /*
     // MARK: - Navigation
@@ -27,5 +31,18 @@ class AddCardViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    @objc func userInputPresent(notification: NSNotification) {
+        var currentStatus = notification.userInfo
+        let insetSize = currentStatus![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect //set inset size to size of keyboard/picker
+        addCardScrollView.contentInset = UIEdgeInsets(top: 0.0, left: 00, bottom: insetSize.height, right: 0.0) //Move Scroll view up to desired inset size
+        addCardScrollView.scrollIndicatorInsets = UIEdgeInsets(top: 0.0, left: 00, bottom: insetSize.height, right: 0.0)
+    }
+    
+    @objc func userInputEnded(notification: NSNotification) {
+        addCardScrollView.contentInset = UIEdgeInsets.zero //Revert Scroll View to previous position
+        addCardScrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
 
 }
--- To-Go/Models/PaymentDetails.swift
+++ To-Go/Models/PaymentDetails.swift
@@ -7,8 +7,33 @@
 //
 
 import Foundation
-
+import UIKit
 
 class PaymentDetails {
+    
+    var cardHolderName: String?
+    var cardNumber: String?
+    var expiryMonth: String?
+    var expiryYear: String?
+    var securityCode: String?
+    
+    init?(cardHolderName: String, cardNumber: String, expiryMonth: String, expiryYear: String, securityCode: String)  {
+        
+        guard !cardNumber.isEmpty || !cardNumber.isEmpty else {
+            return nil
+        }
+        
+        guard !expiryMonth.isEmpty || !expiryYear.isEmpty || !securityCode.isEmpty else {
+            
+            return nil
+        }
+        
+        self.cardHolderName = cardHolderName
+        self.cardNumber = cardNumber
+        self.expiryMonth = expiryMonth
+        self.expiryYear = expiryYear
+        self.securityCode = securityCode
+        
+    }
     
 }
